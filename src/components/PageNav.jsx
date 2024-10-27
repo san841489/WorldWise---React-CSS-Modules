bf5ac711a6a60655ad2411eb2f0325cd882b0ef1
San <san841489@gmail.com>
2024-10-27 22:18:10 +0800

    Building Pages

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,8 +1,11 @@
 import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -12,6 +15,11 @@ function PageNav() {
         </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
